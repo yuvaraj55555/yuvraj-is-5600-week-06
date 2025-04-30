066201ce94b24066e44c968b436f16fa1e4183e5
yuvaraj55555 <saivenkatayuvarajgoud.gajjala@slu.edu>
2025-04-30 03:03:04 +0000

    Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+@@ -1,9 +1,18 @@
 import productData from './data/full-products';
+import { Route, Routes} from 'react-router-dom';
+import Header from './components/Header';
+import CardList from './components/CardList';
+import SingleView from './components/SingleView';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello There.</h1>
+       <Header />
+       <Routes>
+          <Route path="/" element={<CardList data={productData} />} />
+          <Route path="/product/:id" element={<SingleView data={productData} />} />
+        </Routes>
     </div>
   );
 }
-
-export default App;
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,5 +1,62 @@
+@@ -1,5 +1,61 @@
 const CardList = () => {
   return ();
-}
+import Search from './Search';
+import Card from './Card'
+import Button from './Button';
+import React, { useState, useEffect } from "react";
 
-export default CardList;
+
+const CardList = ({data}) => {
+
+  const limit = 10;
+  const defaultDataset = data.slice(0, limit);
+
+  const [offset, setOffset] = useState(0);
+  const [products, setProducts] = useState(defaultDataset);
+
+  const handlePrevious = () => {
+    // set the offset to the previous 10 products
+    setOffset(offset - 10);
+  }
+
+  const handleNext = () => {
+    // set the offset to the next 10 products
+    setOffset(offset + 10);
+  }
+
+  useEffect(() => {
+    // set the products state variable to the next 10 products
+    setProducts(data.slice(offset, offset + limit));
+  }, [offset, limit, data]);
+
+  const filterTags= (tagQuery) => {
+    const filtered = data.filter(product=>{
+      if(!tagQuery){
+        return product
+      }
+      return product.tags.find(({title}) => title === tagQuery)
+    })
+
+    setOffset(0)
+    setProducts(filtered.slice(0,limit))
+  }
+
+
+  return (
+    <div className="cf pa2">
+      <Search handleSearch={filterTags}/>
+      <div className="mt2 mb2">
+
+      {products && products.map((product) => (
+        <Card key={product.id} {...product} />
+      ))}
+      </div>
+
+      <div className="flex items-center justify-center pa4">   
+        <Button text="Previous" handleClick={handlePrevious}/>
+        <Button text="Next" handleClick={handleNext}/>
+      </div>
+    </div>
+  )
+}
